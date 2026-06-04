fd716fcdbe115a3c891b31c1da1271f4813acaa6
melchizedeksarki616-ux <melchizedeksarki616@gmail.com>
2026-06-04 04:44:38 -0700

    first commit

---
+++ dict.py
@@ -0,0 +1,30 @@
+#working with dictionary
+
+# dict = {'Taraba':'Jalingo',
+#         'Sardauna':'Gembu',
+#         'Bali':'bali',
+#         'Kurmi':'Bayelsa'}
+
+#dict.pop('Bali')
+# dict.clear()
+#dict.update({'Ghashaga':'Baruwa'})
+#print(dir(dict))
+#print(help(dict))
+#print(dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n1 = float(input("Enter Number"))
+oper= float(input("Enter Number"))
+n2 = float(input("Enter Number"))
